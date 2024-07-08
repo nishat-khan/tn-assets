1c0602597ee1ca376ed84d78d8d65f46ae486521
nishat-khan <nishatconnect@gmail.com>
2024-07-08 11:20:29 -0700

    schema tried pydantic

---
+++ schema.py
@@ -0,0 +1,41 @@
+# utils/schemas.py
+from typing import List, Optional, Dict, Any
+from pydantic import BaseModel
+
+
+class Tag(BaseModel):
+    key: str
+    value: str
+
+
+class CloudAccount(BaseModel):
+    id: str
+    name: str
+
+
+class Asset(BaseModel):
+    id: str
+    name: str
+    type: str
+    tags: List[Tag]
+    cloud_account: CloudAccount
+    owner_id: str
+    region: str
+    group_name: str = None
+
+
+class Condition(BaseModel):
+    operator: str
+    field: str = None
+    value: str = None
+    key: str = None
+    conditions: List[Any] = None
+
+
+class Rule(BaseModel):
+    conditions: Condition
+
+
+class GroupingRequest(BaseModel):
+    group_name: str
+    rules: List[Rule]
